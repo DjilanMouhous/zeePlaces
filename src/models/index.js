f9aea2ab43171e576c5906b0fc897033ac71ba33
Clément TOLOIS <clement.tolois@gmail.com>
2020-11-08 18:32:35 +0100

    Feat: models relations

--- src/models/index.js
+++ src/models/index.js
@@ -11,32 +11,95 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 // ! Require models
-// db.users = require("./user.model.js")(sequelize, Sequelize);
-// db.events = require("./event.model.js")(sequelize, Sequelize);
-// db.playlists = require("./playlist.model.js")(sequelize, Sequelize);
+db.user = require("./user.model.js")(sequelize, Sequelize);
+db.favourite = require("./favourite.model.js")(sequelize, Sequelize);
+db.place = require("./place.model.js")(sequelize, Sequelize);
+db.fixturePlace = require("./fixturePlace.model.js")(sequelize, Sequelize);
+db.categorie = require("./categorie.model.js")(sequelize, Sequelize);
+db.color = require("./color.model.js")(sequelize, Sequelize);
+db.address = require("./address.model.js")(sequelize, Sequelize);
+db.city = require("./city.model.js")(sequelize, Sequelize);
+
 
 // ! Define relations
+// ? favourite <-> user
+db.favourite.belongsTo(db.user, {
+  foreignKey: 'fk_user_id',
+  as:'user'
+});
+db.user.hasMany(db.favourite, {
+  foreignKey: 'fk_user_id',
+  as:'favourites'
+});
 
-// ? Une playlist a un event
-// ? Un event a des playlists
-// db.playlists.belongsTo(db.events, {
-//   foreignKey: 'fk_event_id',
-//   as: 'event'
-// });
-// db.events.hasMany(db.playlists, {
-//   foreignKey: 'fk_event_id',
-//   as:'playlists'
-// });
+// ? favourite <-> place
+db.favourite.belongsTo(db.place, {
+  foreignKey: 'fk_place_id',
+  as:'place'
+});
+db.place.hasMany(db.favourite, {
+  foreignKey: 'fk_place_id',
+  as:'favourites'
+});
 
-// ? Un event a un user
-// ? Un user a des events
-// db.events.belongsTo(db.users, {
-//   foreignKey: 'fk_user_id',
-//   as: 'user'
-// });
-// db.users.hasMany(db.events, {
-//   foreignKey: 'fk_user_id',
-//   as:'events'
-// });
+// ? fixturePlace <-> place
+db.fixturePlace.belongsTo(db.place, {
+  foreignKey: 'fk_place_id',
+  as:'place'
+});
+db.place.hasMany(db.fixturePlace, {
+  foreignKey: 'fk_place_id',
+  as:'fixturePlaces'
+});
+
+// ? fixturePlace <-> categorie
+db.fixturePlace.belongsTo(db.categorie, {
+  foreignKey: 'fk_categorie_id',
+  as:'categorie'
+});
+db.categorie.hasMany(db.fixturePlace, {
+  foreignKey: 'fk_categorie_id',
+  as:'fixturePlaces'
+});
+
+// ? categorie <-> categorie 
+db.categorie.belongsTo(db.categorie, {
+  foreignKey: 'fk_categorie_id',
+  as:'categorie'
+});
+db.place.hasMany(db.categorie, {
+  foreignKey: 'fk_categorie_id',
+  as:'categories'
+});
+
+// ? categorie <-> color
+db.categorie.belongsTo(db.color, {
+  foreignKey: 'fk_color_id',
+  as:'color'
+});
+db.color.hasMany(db.categorie, {
+  foreignKey: 'fk_color_id',
+  as:'categories'
+});
+
+// ? place <-> address
+db.place.belongsTo(db.address, {
+  foreignKey: 'fk_address_id',
+  as: 'address'
+});
+db.address.hasOne(db.place, {
+  foreignKey: 'fk_address_id',
+  as:'places'
+});
+
+// ? address <-> city
+db.address.belongsTo(db.city, {
+  foreignKey: 'fk_city_id',
+  as:'city'
+});
+db.city.hasMany(db.address, {
+  foreignKey: 'fk_city_id',
+  as:'addresses'
+});
 
 module.exports = db;
