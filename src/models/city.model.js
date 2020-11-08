fac49c106ed95e24ed45e7dfc90762a0b75b6f49
Clément TOLOIS <clement.tolois@gmail.com>
2020-11-08 18:31:56 +0100

    Fix: model name

--- src/models/city.model.js
+++ src/models/city.model.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
-    const City = sequelize.define("place", {
+    const City = sequelize.define("city", {
         label: {
             type: Sequelize.STRING,
             allowNull: false,
